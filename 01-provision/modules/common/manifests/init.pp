32af52ab29dadd8981d7f25206cc6f77004f3e05
Karl Chu <kchu13@gmail.com>
2014-08-13 08:09:03 +1000

    Provision: Change the manifest to use pre-downloaded binaries.

--- 01-provision/modules/common/manifests/init.pp
+++ 01-provision/modules/common/manifests/init.pp
@@ -1,10 +1,3 @@
 class common {
-	$packages_dir = '/root/packages'
-
-	file { $packages_dir:
-		ensure => directory,
-		mode => 0700,
-		owner => 'root',
-		group => 'root',
-	}
+	$packages_dir = '/vagrant/binaries'
 }
--- 01-provision/modules/jdk7/manifests/init.pp
+++ 01-provision/modules/jdk7/manifests/init.pp
@@ -3,22 +3,13 @@ class jdk7 {
 
 	$packages_dir = $common::packages_dir
 
-	$url = 'https://www.dropbox.com/s/ybvwo9r3r68fh0x/jdk-7u67-linux-x64.rpm'
 	$rpm_name = 'jdk-7u67-linux-x64.rpm'
 	$rpm_path = "${packages_dir}/${rpm_name}"
-
-	wget::fetch { $url:
-		destination => $rpm_path,
-		timeout     => 0,
-		verbose     => false,
-		require		=> File[$packages_dir],
-	}
 
 	package { 'jdk7':
 		ensure => installed,
 		provider => 'rpm',
 		source => $rpm_path,
-		require => Wget::Fetch[$url],
 	}
 
 }
