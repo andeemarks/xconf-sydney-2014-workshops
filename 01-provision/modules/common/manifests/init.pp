6766fe94bb296feb40a1e22fe837a911aab2ca8a
Karl Chu <kchu13@gmail.com>
2014-08-24 21:05:20 +1000

    Moved EPEL to common and added the role::client_app wrapper class.

--- 01-provision/modules/common/manifests/init.pp
+++ 01-provision/modules/common/manifests/init.pp
@@ -1,9 +1,9 @@
 class common {
-	$packages_dir = '/vagrant/binaries'
+  $packages_dir = '/vagrant/binaries'
 
-	exec { '/vagrant/binaries/download.sh': 
-		umask => 0022,
-	}
+  exec { '/vagrant/binaries/download.sh':
+    umask => 0022,
+  }
 
   package { ['avahi', 'avahi-tools']:
     ensure => installed
@@ -14,8 +14,14 @@ class common {
     ensure => running
   }
 
-	service { ['iptables', 'ip6tables']:
-		enable => false,
-		ensure => stopped,
-	}
+  service { ['iptables', 'ip6tables']:
+    enable => false,
+    ensure => stopped,
+  }
+
+  package { 'epel-release-6-8':
+    ensure => present,
+    provider => 'rpm',
+    source => 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm',
+  }
 }
