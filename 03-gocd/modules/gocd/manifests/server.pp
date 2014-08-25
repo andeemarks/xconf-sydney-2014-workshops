c7d9742a9cfab6772f4b0c6ca3d5c6fb1133013f
Romit Kala <rkala@thoughtworks.com>
2014-08-25 18:27:54 +1000

    Romit: added ci gocd presentation material

---
+++ 03-gocd/modules/gocd/manifests/server.pp
@@ -0,0 +1,36 @@
+class gocd::server {
+
+	include common
+	include jdk7
+
+	$packages_dir = $common::packages_dir
+
+	$version = '14.2.0-377'
+	$package_name = "go-server-${version}"
+	$binary_name = "${package_name}.noarch.rpm"
+	$binary_path = "${packages_dir}/${binary_name}"
+
+	package { $package_name:
+		ensure => installed,
+		provider => 'rpm',
+		source => $binary_path,
+	}
+
+	file_line { 'Set JAVA_HOME for go server':
+		path  => '/etc/default/go-server',
+		line  => 'JAVA_HOME=/usr/java/default',
+		match => '^JAVA_HOME=.*',
+		require => Package[$package_name],
+		notify => Service['go-server'],
+	}
+
+	service { 'go-server':
+		enable => true,
+		ensure => running,
+		require => [ 
+			Package[$package_name], 
+			Class['jdk7'], 
+			File_line['Set JAVA_HOME for go server'],
+		],
+	}
+}
