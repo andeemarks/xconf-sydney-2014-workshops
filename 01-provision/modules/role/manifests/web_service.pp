bdbecbe0b68e30d69b7682657f18a34fe6be66b3
Karl Chu <kchu13@gmail.com>
2014-08-26 23:04:53 +1000

    Added ruby 1.9.3 rpm package install

--- 01-provision/modules/role/manifests/web_service.pp
+++ 01-provision/modules/role/manifests/web_service.pp
@@ -3,8 +3,9 @@ class role::web_service {
   include common
   include tw_collectd
 
-  rbenv::install { '2.0.0-p481':
-    ruby_version => '2.0.0-p481',
+  package { 'ruby-ioof':
+    provider => 'rpm',
+    source => '/vagrant/binaries/ruby-1.9.3p484-2.el6.x86_64.rpm',
   }
 
   Class['common']->Class['tw_collectd']
