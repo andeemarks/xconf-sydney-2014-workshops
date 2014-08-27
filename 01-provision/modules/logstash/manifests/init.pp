086cd91e683eec907fca653ae6d84cd9d0794812
Mia Zhu <zxx1110@hotmail.com>
2014-08-27 22:49:46 +1000

    specify package running order

--- 01-provision/modules/logstash/manifests/init.pp
+++ 01-provision/modules/logstash/manifests/init.pp
@@ -2,7 +2,7 @@ class logstash{
 
 	include common
 
-	package { 'java-1.6.0-openjdk': ensure => installed }
+	
 
 	$packages_dir = $common::packages_dir
 
@@ -10,6 +10,7 @@ class logstash{
 	$rpm_name = "logstash-1.4.2-1_2c0f5a1.noarch.rpm"
 	$rpm_path = "${packages_dir}/${rpm_name}"
 
+	package { 'java-1.6.0-openjdk': ensure => installed } -> 
 	package { $package_name:
 		ensure => installed,
 		provider => 'rpm',
