6193b6201fac09184d6594a22ecde528ef8854b5
Karl Chu <kchu13@gmail.com>
2014-08-27 23:44:41 +1000

    Added an exec resource to install riemann-dash gem under ruby 1.9.3

--- 01-provision/modules/tw_riemann/manifests/init.pp
+++ 01-provision/modules/tw_riemann/manifests/init.pp
@@ -11,6 +11,18 @@ class tw_riemann {
  # ->
  # class { '::riemann::tools' :
  # }
+
+  exec { 'install riemann-dash gem under ruby 1.9.3':
+    path => ['/opt/ruby/ruby193/bin'],
+    command => 'gem install riemann-dash',
+    environment => [
+      'GEM_HOME=/opt/ruby/ruby193/lib64/ruby/gems/1.9.1',
+      'GEM_PATH=/opt/ruby/ruby193/lib64/ruby/gems/1.9.1',
+      'LD_LIBRARY_PATH=/opt/ruby/ruby193/lib64',
+    ],
+    unless => 'gem list | /bin/grep -E "^riemann-dash "',
+    require => [ Class['::riemann::dash'], Class['::ruby::v193'] ],
+  }
 }
 
   
