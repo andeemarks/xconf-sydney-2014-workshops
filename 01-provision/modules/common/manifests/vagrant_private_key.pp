a00a581888dab26eeaaa661cae3ecaf666d5e016
Karl Chu <kchu13@gmail.com>
2014-08-28 21:38:43 +1000

    Added private key to allow the vagrant user from any box to ssh into any other box.

---
+++ 01-provision/modules/common/manifests/vagrant_private_key.pp
@@ -0,0 +1,16 @@
+class common::vagrant_private_key {
+  file { '/home/vagrant/.ssh':
+    ensure => directory,
+    owner => 'vagrant',
+    group => 'vagrant',
+    mode => 0700,
+  }
+
+  file { '/home/vagrant/.ssh/id_rsa':
+    ensure => file,
+    owner => 'vagrant',
+    group => 'vagrant',
+    mode => 0400,
+    source => 'puppet:///modules/common/vagrant/id_rsa',
+  }
+}
